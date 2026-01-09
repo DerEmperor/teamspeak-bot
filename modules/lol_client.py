fac58fd87d2a271f095cb4ba3ecc61e195fe7829
Tobias Kaiser <tobias.nuernberg@web.de>
2026-01-09 18:37:38 +0100

    games have a 3min delay

--- modules/lol_client.py
+++ modules/lol_client.py
@@ -287,7 +287,7 @@ class LolUser:
         return LolGame(
             game_id=game['gameId'],
             mode=translate_lol_mode(game['gameMode'], game['gameType'], game['gameQueueConfigId']),
-            time=game['gameLength'],
+            time=game['gameLength'] + 3 * 60,  # there is a 3m delay
             ts_participants=ts_participants,
             lol_participants=lol_participants,
             banned_champions=banned_champs,
